1b8c23bdbc6d9a9a77c60e0f2780272b9f556b8f
ahuxford <ahuxford@umich.edu>
2020-07-16 13:21:09 -0400

    updated NekML.f file

--- files/NekML.f
+++ files/NekML.f
@@ -43,3 +43,45 @@ c set arguments
       call args1%destroy
       
       end
+c---------------------------------------------------------------------
+c NekML_serialtrain
+c
+c save vx to a .h5 file, serves as an example for than anything
+c - to modify, just change ndarray_create(arr,___) variable
+c - change args1%setitem(2, "___") string
+c
+c---------------------------------------------------------------------
+      subroutine NekML_serialtrain()
+
+      use forpy_mod
+      include 'SIZE'
+      include 'SOLN'
+      include 'TSTEP'
+    
+      integer         :: ierror
+      type(tuple)     :: args1
+      type(module_py) :: mymodule
+      type(list)      :: paths
+      type(ndarray)   :: arr_in, arr_out
+
+      ierror = forpy_initialize()
+
+      ierror = ndarray_create(arr_in,vx)  !  input of neural network
+      ierror = ndarray_create(arr_out,vy) ! output of neural network
+
+c add directory with NekML Python modules to PYTHONPATH
+      ierror = get_sys_path(paths)
+      ierror = paths%append("NekML_PyMods")
+      ierror = import_py(mymodule, "NekML")
+
+c set arguments 
+      ierror = tuple_create(args1, 3)
+      ierror = args1%setitem(0, arr_in)
+      ierror = args1%setitem(1, arr_out)
+      ierror = args1%setitem(2, istep)
+
+      ierror = call_py_noret(mymodule, "serial_train",args1)
+
+      call args1%destroy
+      
+      end
